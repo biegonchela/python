bd41ec8c51b7188928c8e8e9f5ca39c21e871c53
Yvonne Biegon <biegonchela@gmail.com>
2025-02-08 11:43:09 +0300

    Add files via upload

---
+++ List EXAM.py
@@ -0,0 +1,10 @@
+mylist = ["blue", 12, True]
+
+print(f"My favorite color is {mylist[0]}.")
+print(f"I have {mylist[1]} pets.")
+
+if mylist[2]==True :
+    print(f"I do have previous programming experience")
+else:
+    print(f"I do not have previous programming experience")
+
