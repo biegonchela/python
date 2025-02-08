2f2675ab6ba4ed2c40a2bca03f26b50a9ed3becf
Yvonne Biegon <biegonchela@gmail.com>
2025-02-08 11:41:46 +0300

    Add files via upload

---
+++ cost_of_ice.py
@@ -0,0 +1,6 @@
+cost_of_ice=1.25
+profit_margin=0.2
+number_of_bags=500
+profit_per_bag=cost_of_ice*profit_margin
+total_profit=number_of_bags*profit_per_bag
+print(f"grocery makes a total of ${total_profit:.2f}")
